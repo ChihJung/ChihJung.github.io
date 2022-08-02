0d7184f77d12679eb1818edbef0150c5f41fbbf0
Melody <b228103056@tmu.edu.tw>
2022-08-03 07:44:45 +0800

    [todoList]

--- JavaScript/10TodoList/script.js
+++ JavaScript/10TodoList/script.js
@@ -1,7 +1,5 @@
 // 宣告
 let todoArray = []
-let listIndex = 0
-let inputVal
 
 
 // DOM
@@ -9,7 +7,8 @@ const todoGroup = document.querySelector('#todo-group')
 const addBtn = document.querySelector('#addBtn')
 const inputText = document.querySelector('.input-group input')
 const todoItemTemp = document.querySelector('#todoItem-template')
-let editBtn, delBtn
+
+let editBtn, delBtn, saveBtn, todoTitle, checkBox
 
 
 
@@ -19,112 +18,112 @@ window.onload = function () {
     if (localStorage.getItem('todoList') != null) {
         setTodoGroup()
     }
-    addBtn.addEventListener('click', addTodo)
-
+    addBtn.addEventListener('click', () => {
+        if (inputText.value == '' || inputText.value == null || inputText.value == undefined) {
+            return
+        } else {
+            addTodo()
+        }
+    })
+    
+    inputText.addEventListener("keypress", function(event) {
+      if (event.key === "Enter") {
+        event.preventDefault()
+        addBtn.click()
+      }
+    })
 }
 
 
 
 
 // function
-function addTodo() {
-    // console.log('i clicked addBtn')
-
-
-
-    inputVal = inputText.value
-    todoGroup.append(setTodoTemplate(inputVal, false))
-
-    // setTodoGroup()
-
-
-    inputText.value = ''
-}
-
-function editTodo(event) {
-    // console.log('i clicked eidtBtn')
-    event.target.parentElement.querySelector('#todoTitle').disabled = false
-    event.target.style.display = 'none'
-    event.target.parentElement.querySelector('#saveBtn').style.display = 'block'
-    inputVal = event.target.parentElement.querySelector('#todoTitle').value
-
-
-    saveBtn.addEventListener('click', () => {
-
-    })
-}
-
-function saveTodo(event) {
-    // event.target.parentElement.querySelector('#todoTitle').disabled = true
-    // event.target.style.display = 'none'
-    // event.target.parentElement.querySelector('#editBtn').style.display = 'block'
-
-
-
-    let todoObj
-    if (localStorage.getItem('todoList') != null) {
-        let storageRec = JSON.parse(localStorage.getItem('todoList'))
-        let removeIndex = storageRec.filter(x => x.title == inputVal).map(x => x.index)
-        if (removeIndex.length == 0) {
-            todoArray = JSON.parse(localStorage.getItem('todoList'))
-            listIndex = parseInt(todoArray[todoArray.length - 1].index) + 1
-            todoObj = { index: `${listIndex++}`, title: `${inputVal}`, checkBoxStatus: false }
-            todoArray.push(todoObj)
-        } else if (removeIndex.length != 0) {
-
-            let newInputVal = event.target.parentElement.querySelector('#todoTitle').value
-
-            let todoObj = { index: `${removeIndex}`, title: `${newInputVal}`, checkBoxStatus: `${storageRec[removeIndex[0]].checkBoxStatus}` }
-            todoArray = storageRec.splice(removeIndex[0], 1, todoObj)
-            // todoArray = todoArray.splice(todoObj, removeIndex[0], 1)
-        }
-    } else {
-        todoObj = { index: `${listIndex++}`, title: `${inputVal}`, checkBoxStatus: false }
-        todoArray.push(todoObj)
-    }
-    localStorage.setItem('todoList', JSON.stringify(todoArray))
-    setTodoGroup()
-}
-
-function delTodo(event) {
-
-}
-
 function setTodoGroup() {
     let todoItems = JSON.parse(localStorage.getItem('todoList'))
     todoGroup.innerHTML = ''
-    todoItems.forEach(item => {
-        todoGroup.append(setTodoTemplate(item.title, item.checkBoxStatus))
-    })
-    document.querySelectorAll('#todoTitle').forEach(item => {
-        item.disabled = true
-    })
-    document.querySelectorAll('#saveBtn').forEach(item => {
-        item.style.display = 'none'
-    })
-    document.querySelectorAll('#editBtn').forEach(item => {
-        item.style.display = 'block'
+    todoItems.forEach((item, index) => {
+        todoGroup.append(setTodoTemplate(item.title, item.checkBoxState, index))
+
+        // btn/input 狀態
+        todoTitle.setAttribute('disabled', '')
+        saveBtn.classList.add('d-none')
+
+
     })
 }
 
-
-function setTodoTemplate(title, checkVal) {
+function setTodoTemplate(title, checkVal, index) {
     let templateClone = todoItemTemp.content.cloneNode(true)
     templateClone.querySelector('#todoTitle').value = title
-    templateClone.querySelector('#todoTitle').disabled = false
-    templateClone.querySelector('#checkStatus').checked = checkVal
+    templateClone.querySelector('#todoTitle').removeAttribute('disabled')
+    templateClone.querySelector('#checkState').checked = checkVal
 
+    todoTitle = templateClone.querySelector('#todoTitle')
     editBtn = templateClone.querySelector('#editBtn')
     saveBtn = templateClone.querySelector('#saveBtn')
     delBtn = templateClone.querySelector('#delBtn')
+    checkBox = templateClone.querySelector('#checkState')
 
-    saveBtn.addEventListener('click', saveTodo)
-    delBtn.addEventListener('click', delTodo)
-    editBtn.addEventListener('click', editTodo)
+    todoTitle.setAttribute('input', index)
+    saveBtn.setAttribute('save', index)
+    delBtn.setAttribute('del', index)
+    editBtn.setAttribute('edit', index)
+    checkBox.setAttribute('check', index)
 
-
+    saveBtn.addEventListener('click', saveTodo.bind(event, index))
+    delBtn.addEventListener('click', delTodo.bind(event, index))
+    editBtn.addEventListener('click', editTodo.bind(event, index))
+    checkBox.addEventListener('click', checkboxChange.bind(event, index))
 
     return templateClone
 }
 
-// addClick -> 渲染畫面 -> oldevent出現 編輯/刪除 -> 新增的出現 儲存/刪除
+
+
+// (btns) event
+function addTodo() {
+
+    if (localStorage.getItem('todoList') != null) {
+        todoArray = JSON.parse(localStorage.getItem('todoList'))
+    }
+
+    let inputVal = inputText.value
+    let todoObj
+    todoObj = { title: `${inputVal}`, checkBoxState: false }
+    todoArray.push(todoObj)
+    localStorage.setItem('todoList', JSON.stringify(todoArray))
+
+    setTodoGroup()
+    inputText.value = ''
+}
+
+
+function editTodo(idx) {
+    document.querySelector(`[edit="${idx}"]`).classList.add('d-none')
+    document.querySelector(`[save="${idx}"]`).classList.remove('d-none')
+    document.querySelector(`[input="${idx}"]`).removeAttribute('disabled')
+}
+
+function saveTodo(idx) {
+    let currentInput = document.querySelector(`[input="${idx}"]`)
+    let todoItems = JSON.parse(localStorage.getItem('todoList'))
+    todoItems[idx].title = currentInput.value
+    localStorage.setItem('todoList', JSON.stringify(todoItems))
+    setTodoGroup()
+}
+
+function delTodo(idx) {
+    let todoItems = JSON.parse(localStorage.getItem('todoList'))
+    todoItems.splice(idx, 1)
+    localStorage.setItem('todoList', JSON.stringify(todoItems))
+    setTodoGroup()
+}
+
+function checkboxChange(idx) {
+    let todoItems = JSON.parse(localStorage.getItem('todoList'))
+    todoItems[idx].checkBoxState = document.querySelector(`[check="${idx}"]`).checked
+    localStorage.setItem('todoList', JSON.stringify(todoItems))
+    setTodoGroup()
+}
+
+
